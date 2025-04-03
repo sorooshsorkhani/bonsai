94f03c197f64d5b9b1fff418919279df363b0571
sorooshsorkhani <soroosh.sorkhani@gmail.com>
2025-04-03 17:55:49 +0000

    added token stream from RAG node to example_run.py

--- app/backend/example_run.py
+++ app/backend/example_run.py
@@ -1,18 +1,33 @@
 import pprint
 from app.backend.graph.rag_graph import create_rag_graph
 
-inputs = {
-    "messages": [
-        ("user", "I need a tool in BON in a Box that can help me with the study of species population."
-        "Also, whom can I contact to ask more about the tool?"),
-    ]
-}
 
-graph = create_rag_graph()
+def nodes_output(graph, inputs):
+    for output in graph.stream(inputs):
+        for key, value in output.items():
+            pprint.pprint(f"Output from node '{key}':")
+            pprint.pprint("---")
+            pprint.pprint(value, indent=2, width=80, depth=None)
+        pprint.pprint("\n---\n")
 
-for output in graph.stream(inputs):
-    for key, value in output.items():
-        pprint.pprint(f"Output from node '{key}':")
-        pprint.pprint("---")
-        pprint.pprint(value, indent=2, width=80, depth=None)
-    pprint.pprint("\n---\n")
+
+def node_tokens(graph, inputs):
+    for msg, metadata in graph.stream(
+        inputs,
+        stream_mode="messages",
+    ):
+        if msg.content and metadata["langgraph_node"] == "rag":
+            print(msg.content, end="", flush=True)
+
+
+if __name__ == "__main__":
+    
+    inputs = {
+        "messages": [
+            ("user", "What is an EBV? Make an example too."),
+        ]
+    }
+    graph = create_rag_graph()
+
+    #nodes_output(graph, inputs)
+    node_tokens(graph, inputs)
