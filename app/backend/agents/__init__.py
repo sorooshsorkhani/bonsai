e28850e07e317a7945fceffdd8a0db5e4d469b39
sorooshsorkhani <soroosh.sorkhani@gmail.com>
2025-04-08 19:01:27 +0000

    added sufficiency, need to continue the work

--- app/backend/agents/__init__.py
+++ app/backend/agents/__init__.py
@@ -1,6 +1,6 @@
 from .agent_state import AgentState
 from .gateway import gateway
-from .grade_documents import grade_docs
+from .grade_documents import grade_relevance, grade_sufficiency
 from .rag import rag
 from .rewrite import rewrite
 from .greet import greet
--- app/backend/agents/grade_documents.py
+++ app/backend/agents/grade_documents.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel, Field
 from langchain_core.prompts import PromptTemplate
 
 
-def grade_docs(state) -> Literal["rag", "greetings"]:
+def grade_relevance(state) -> Literal["grade_sufficiency", "greetings"]:
     """
-    Determines whether the retrieved documents are relevant to the question.
+    Determines whether the retrieved documents are relevant to the query.
 
     Args:
         state (messages): The current state
@@ -24,38 +24,96 @@ def grade_docs(state) -> Literal["rag", "greetings"]:
         binary_score: str = Field(description="Relevance score 'yes' or 'no'")
 
     # LLM
-    grade_model = GroqLLM.load_llm()
+    relevance_model = GroqLLM.load_llm()
 
     # LLM with tool and validation
-    llm_with_tool = grade_model.with_structured_output(grade)
+    llm_with_relevance = relevance_model.with_structured_output(grade)
 
     # Prompt
     prompt = PromptTemplate(
-        template="""You are a grader assessing relevance of retrieved documents to a user question. \n 
+        template="""You are a grader assessing relevance of retrieved documents to a user query. \n 
         Here is the retrieved documents: \n\n {context} \n\n
-        Here is the user question: {question} \n
-        If the documents contain keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
-        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.""",
-        input_variables=["context", "question"],
+        Here is the user query: {query} \n
+        If the documents contain keyword(s) or semantic meaning related to the user query, grade it as relevant. \n
+        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the query.""",
+        input_variables=["context", "query"],
     )
 
     # Chain
-    chain = prompt | llm_with_tool
+    relevance_chain = prompt | llm_with_relevance
 
     messages = state["messages"]
-    question = messages[0].content
+    query = messages[0].content
     last_message = messages[-1]
     docs = last_message.content
 
-    scored_result = chain.invoke({"question": question, "context": docs})
+    scored_result = relevance_chain.invoke({"query": query, "context": docs})
 
     score = scored_result.binary_score
 
     if score == "yes":
         print("---DECISION: DOCS RELEVANT---")
-        return "rag"
+        return "grade_sufficiency"
 
     else:
         print("---DECISION: DOCS NOT RELEVANT---")
         print(score)
         return "greetings"
+
+
+def grade_sufficiency(state) -> Literal["rag", "additional_retrieve"]:
+    """
+    Determines whether the retrieved documents are sufficient to answer the query.
+
+    Args:
+        state (messages): The current state
+
+    Returns:
+        str: A decision for whether the documents are sufficient or not
+    """
+
+    print("---CHECK SUFFICIENCY---")
+
+    # Data model
+    class grade(BaseModel):
+        """Binary score for sufficiency check."""
+
+        binary_score: str = Field(description="Sufficiency score 'yes' or 'no'")
+
+    # LLM
+    sufficiency_model = GroqLLM.load_llm()
+
+    # LLM with tool and validation
+    llm_with_sufficiency = sufficiency_model.with_structured_output(grade)
+
+    # Prompt
+    prompt = PromptTemplate(
+        template="""You are a grader assessing sufficiency of retrieved documents to a user query. \n 
+        Here is the retrieved documents: \n\n {context} \n\n
+        Here is the user query: {query} \n
+        If the documents sufficient to answer the user query and no additional context is necessary, grade it as sufficient. \n
+        Give a binary score 'yes' or 'no' score to indicate whether the documents are sufficient.""",
+        input_variables=["context", "query"],
+    )
+
+    # Chain
+    sufficiency_chain = prompt | llm_with_sufficiency
+
+    messages = state["messages"]
+    query = messages[0].content
+    last_message = messages[-1]
+    docs = last_message.content
+
+    scored_result = sufficiency_chain.invoke({"query": query, "context": docs})
+
+    score = scored_result.binary_score
+
+    if score == "yes":
+        print("---DECISION: DOCS SUFFICIENT---")
+        return "rag"
+
+    else:
+        print("---DECISION: DOCS NOT SUFFICIENT---")
+        print(score)
+        return "additional_retrieve"
+
